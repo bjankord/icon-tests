076b6741cc782af7370a7a0a24e5001b9e755224
Brett Jankord <Brett.Jankord@Cerner.com>
2016-02-01 13:36:57 -0600

    Fix issues with IE9

--- svg-tests/test7/sprite.js
+++ svg-tests/test7/sprite.js
@@ -1,13 +1,8 @@
 var ajax = new XMLHttpRequest();
 ajax.open("GET", "sprite.svg", true);
-ajax.responseType = "document";
-ajax.onload = function(e) {
-  try {
-    svg = ajax.responseXML.documentElement;
-    svg.setAttribute("class", "sprite");
-    //not essential, just used to trigger CSS to hide the SVG
-    document.body.insertBefore(svg, document.body.childNodes[0]);
-  }
-  catch(e){console.log(e);}
-}
 ajax.send();
+ajax.onload = function(e) {
+  var div = document.createElement("div");
+  div.innerHTML = ajax.responseText;
+  document.body.insertBefore(div, document.body.childNodes[0]);
+}
